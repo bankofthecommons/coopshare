002cb514d66a00730a0a4adc0b03a5cd5c223663
vmatekole <victor@matekole.com>
2018-07-26 03:17:44 +0200

    compile faircoin with wallet enabled + some fixes for berkeley db + remove redundant files

--- backend/test.sh
+++ backend/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "account-id": "mtJbXVjX1CSJnoMZb6PkzunABMgMCT4mrP"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/balance'
-#curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "account-id": "mtJbXVjX1CSJnoMZb6PkzunABMgMCT4mrP"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/list'
-# curl -vvvv  -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "address": "fMbMaWFkBkmCM6NctGSzRs5CY3fB2SQaL9"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/deposits/check'
+
+    curl -vvvv --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "account-id": "fMbMaWFkBkmCM6NctGSzRs5CY3fB2SQaL9"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/balance'
+curl -vvvv --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "account-id": "fMbMaWFkBkmCM6NctGSzRs5CY3fB2SQaL9"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/list'
+curl -vvvv  --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "address": "fMbMaWFkBkmCM6NctGSzRs5CY3fB2SQaL9"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/deposits/check'
 # curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "address": "mtJbXVjX1CSJnoMZb6PkzunABMgMCT4mrP"}' 'http://freecoin.seedbloom.it:2100/wallet/v1/deposits/check'
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "txid": "e80c8aa284e752a2a8d6221ced6bad8514e13f373dd16dedc6b172e01890a5d8"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/get'
+#curl -vvvv -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "blockchain": "faircoin", "txid": "e80c8aa284e752a2a8d6221ced6bad8514e13f373dd16dedc6b172e01890a5d8"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/get'
 #curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"blockchain": "faircoin", "txid": "817e9719bc58496f8220f24a9ccffb8f8823aaeea4dbc49087ee14b1ca49610c"}' 'http://freecoin.seedbloom.it:2100/wallet/v1/transactions/get'
 
 
 
-curl -vvvv -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
-   "blockchain": "faircoin"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/list'
+#curl -vvvv -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+#   "blockchain": "faircoin"}' 'http://freecoin.seedbloom.it:2000/wallet/v1/transactions/list'
